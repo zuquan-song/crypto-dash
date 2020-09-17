c88edd3fcfc1b95842a9143fe3dd9d2939e38da0
zuquan-song <zs1243@nyu.edu>
2020-09-17 10:47:42 -0400

    add font

---
+++ src/App/WelcomeMessage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Welcome = (props) => {
+  return <h1>Welcome to CryptoDash </h1>
+}
+
+export default Welcome;
--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import './App.css';
+import Welcome from './WelcomeMessage';
+import styled, {css} from 'styled-components';
+
+const MyButton = styled.div`
+  color: green;
+
+  ${props => props.primary && css`
+    color: palevioletred;
+  `}
+`
+
+const TomatoButton = styled(MyButton)`
+  color: tomato;
+  border-color: tomato;
+`;
 
 function App() {
   return (
-    <div>Hello world</div>
+    <div>
+      <Welcome />
+      <MyButton> Hello </MyButton>
+      <MyButton primary> Hello </MyButton>
+      <TomatoButton primay> Hello </TomatoButton>
+    </div>
   );
 }
 
