9d75a1eb3bd9f050a8f60d124d194a9f031fba17
zuquan-song <zs1243@nyu.edu>
2020-09-17 15:59:40 -0400

    add cointile

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -23,7 +23,6 @@ export class AppProvider extends React.Component {
 	fetchCoins = async () => {
 		let coinList = (await cc.coinList()).Data
 		this.setState({coinList})
-		console.log(coinList);
 	}
 
 	confirmFavorites = () => {
